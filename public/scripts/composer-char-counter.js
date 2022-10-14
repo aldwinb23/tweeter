d15b531a8ff017d1fd3cef2911c3c3eb3abf7aa6
aldwinb <aldwinb23@gmail.com>
2022-10-13 21:54:17 -0700

    refactor and delete console.logs

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,13 +5,9 @@ $(document).ready(function() {
     const maxNumOfChars = 140;
     const $charCount = $("output.counter");
 
-    // const $charCount = $(this).next().output.counter;
-
-
     let numOfEnteredChars = $(this).val().length;
     let counter = maxNumOfChars - numOfEnteredChars;
-    $charCount.text(counter)
-
+    $charCount.text(counter);
 
     if (counter < 0) {
       $charCount.css("color", "red");
